bc9abc1e8420815d3ae9040ea7f5e3738f723815
DaesanLee <kdb2013@g.skku.edu>
2022-04-25 23:50:43 +0900

    commit1

---
+++ main.js
@@ -0,0 +1,20 @@
+num1 = 3;
+
+var num = 10;
+console.log(num);
+
+let num2 = 10;
+console.log(num2);
+
+const number = 10;
+const number = 20;
+
+console.log(number);
+
+let number =1;
+
+let infinity = 10/0;
+let nan = 10/ "칠";
+console.log(infinity,nan);
+
+let str = "i'm fine";
